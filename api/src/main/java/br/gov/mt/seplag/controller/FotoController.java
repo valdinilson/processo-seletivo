a50b9b3999eec5602c401d5851d910d0f3091c85
Valdinilson Lourenço da Cunha <valdinilson@gmail.com>
2025-03-31 18:52:59 -0400

    Classe controller ajustada para melhor retorno e recepção dos arquivos vindos do Swagger.

--- api/src/main/java/br/gov/mt/seplag/controller/FotoController.java
+++ api/src/main/java/br/gov/mt/seplag/controller/FotoController.java
@@ -1,16 +1,20 @@
 package br.gov.mt.seplag.controller;
 
-import br.gov.mt.seplag.domain.Foto;
 import br.gov.mt.seplag.service.FotoService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Tag(name = "Servidores")
 @RestController
@@ -21,11 +25,21 @@ public class FotoController {
     private final FotoService fotoService;
 
     @Operation(summary = "Registrar o upload de um ou mais fotos para o Servidor")
-    @ApiResponse(responseCode = "200", description = "Sucesso")
+    @ApiResponse(responseCode = "201", description = "Sucesso")
     @ApiResponse(responseCode = "404", description = "Não encontrada")
     @ApiResponse(responseCode = "500", description = "Erro Interno")
-    @PostMapping("/servidores/{id}/fotos")
-    public ResponseEntity<List<Foto>> uploadFotos(@PathVariable Integer id, @RequestParam List<MultipartFile> arquivos) {
-        return ResponseEntity.ok(fotoService.uploadFotos(id, arquivos));
+    @PostMapping(value = "/servidores/{id}/fotos", consumes = "multipart/form-data")
+    public ResponseEntity<Object> uploadFotos(
+            @PathVariable Integer id,
+            @Parameter(description = "Lista de fotos a serem enviados",
+                    content = @Content(mediaType = "multipart/form-data"), required = true)
+            @RequestParam("imagem") List<MultipartFile> imagens) {
+
+        List<Map<String, Integer>> ids = fotoService.uploadFotos(id, imagens).stream()
+                .map(foto -> Map.of("id", foto.getId()))
+                .collect(Collectors.toList());
+
+        return ResponseEntity.
+                status(HttpStatus.CREATED).body(ids);
     }
 }
